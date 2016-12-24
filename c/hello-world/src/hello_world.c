640e2e438c9f23fe22e8a1e4b7c78324fcd2b034
Jason Benn <jasoncbenn@gmail.com>
2016-12-24 15:43:40 -0500

    [hello-world] I am so confused by this result.

--- c/hello-world/src/hello_world.c
+++ c/hello-world/src/hello_world.c
@@ -2,6 +2,31 @@
 #include <stddef.h>
 #include "hello_world.h"
 
-void hello(char *buffer, const char *name)
-{
+void hello(char *buffer, const char *name) {
+    printf("Hello ");
+    if (name) {
+        for (int i = 0; name[i] != '\0' && i < *buffer; ++i) {
+            printf("%c", name[i]);
+        }
+    } else {
+        printf("World");
+    }
+    printf("!\n");
+}
+
+int main(void) {
+    char buffer[5];
+    hello(buffer, "A");
+    hello(buffer, "AB");
+    hello(buffer, "ABC");
+    hello(buffer, "ABCD");
+    hello(buffer, "ABCDE");
+    hello(buffer, "ABCDEF");
+    hello(buffer, "ABCDEFG");
+    hello(buffer, "ABCDEFGH");
+    hello(buffer, "ABCDEFGHI");
+    hello(buffer, "ABCDEFGHIJ");
+    hello(buffer, "ABCDEFGHIJK");
+    hello(buffer, "ABCDEFGHIJKL");
+    hello(buffer, NULL);
 }
