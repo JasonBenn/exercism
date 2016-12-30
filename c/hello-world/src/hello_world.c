3e0871fc0644a5a1a6e4732f5262e01be4d27281
Jason Benn <jasoncbenn@gmail.com>
2016-12-30 11:54:48 -0500

    Fix hello_world using stdlib - fill the buffer with a formatted string via sprintf.

--- c/hello-world/src/hello_world.c
+++ c/hello-world/src/hello_world.c
@@ -1,32 +1,12 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 #include "hello_world.h"
 
 void hello(char *buffer, const char *name) {
-    printf("Hello ");
     if (name) {
-        for (int i = 0; name[i] != '\0' && i < *buffer; ++i) {
-            printf("%c", name[i]);
-        }
+        sprintf(buffer, "Hello, %s!", name);
     } else {
-        printf("World");
+        sprintf(buffer, "Hello, World!");
     }
-    printf("!\n");
-}
-
-int main(void) {
-    char buffer[5];
-    hello(buffer, "A");
-    hello(buffer, "AB");
-    hello(buffer, "ABC");
-    hello(buffer, "ABCD");
-    hello(buffer, "ABCDE");
-    hello(buffer, "ABCDEF");
-    hello(buffer, "ABCDEFG");
-    hello(buffer, "ABCDEFGH");
-    hello(buffer, "ABCDEFGHI");
-    hello(buffer, "ABCDEFGHIJ");
-    hello(buffer, "ABCDEFGHIJK");
-    hello(buffer, "ABCDEFGHIJKL");
-    hello(buffer, NULL);
 }
